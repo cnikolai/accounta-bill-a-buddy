0a5712610e01cce0aa23e97569a0f1b32e8ac0be
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-06-16 11:46:45 -0700

    integrated firebase cocoapods into project

---
+++ accounta-bill-a-buddy/Pods/Target Support Files/Pods-accounta-bill-a-buddy/Pods-accounta-bill-a-buddy-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_accounta_bill_a_buddyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_accounta_bill_a_buddyVersionString[];
+
