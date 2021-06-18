0089dd0a76fcb2977556c993b7a8c4382454b80c
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-06-18 08:22:16 -0700

    working on implementing firebase auth

--- accounta-bill-a-buddy/accounta-bill-a-buddy/Extentions and Helpers/ExtentionsAndHelpersPlaceholder.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ExtentionsAndHelpersPlaceholder.swift
-//  accounta-bill-a-buddy
-//
-//  Created by Cynthia Nikolai on 6/16/21.
-//
-
-import Foundation
---
+++ accounta-bill-a-buddy/accounta-bill-a-buddy/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  accounta-bill-a-buddy
+//
+//  Created by Jenny Morales on 6/16/21.
+//
+
+import Foundation
+
+class User {    
+    
+}//End of class
