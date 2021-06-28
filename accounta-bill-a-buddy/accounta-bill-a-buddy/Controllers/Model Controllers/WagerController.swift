7c52fe759fcf1dd6f373936dd9bf5b9eac22ad27
rishikolappa <rishik@gmail.com>
2021-06-28 15:44:21 -0700

    Project Update

--- accounta-bill-a-buddy/accounta-bill-a-buddy/Controllers/Model Controllers/WagerController.swift
+++ accounta-bill-a-buddy/accounta-bill-a-buddy/Controllers/Model Controllers/WagerController.swift
@@ -21,7 +21,7 @@ class WagerController {
         let newWager = Wager(owner: "rishikolappa@gmail.com", wagerBuddies: ["test@test.org", "test@test.com"], wagerPhoto: UIImage(named: "soccer"), goalDescription: "soccer", wager: "Test Wager", deadline: "Test date", progress: 0.3, uuid: "001")
         wagers.append(newWager)
         
-        let newWager2 = Wager(owner: "rishikolappa@gmail.com", wagerBuddies: ["test@test.org", "test@test.com"], wagerPhoto: UIImage(named: "football"), goalDescription: "football", wager: "Test Wager", deadline: "Test date", progress: 0.5, uuid: "002")
+        let newWager2 = Wager(owner: "rishikolappa@gmail.com", wagerBuddies: ["test@test.org", "test@test.com"], goalDescription: "football", wager: "Test Wager", deadline: "Test date", progress: 0.5, uuid: "002")
         wagers.append(newWager2)
         
         let newWager3 = Wager(owner: "rishikolappa@gmail.com", wagerBuddies: ["test@test.org", "test@test.com"], wagerPhoto: UIImage(named: "basketball"), goalDescription: "basketball", wager: "Test Wager", deadline: "Test date", progress: 0.7, uuid: "003")
@@ -33,44 +33,82 @@ class WagerController {
         let newWager5 = Wager(owner: "rishikolappa@gmail.com", wagerBuddies: ["test@test.org", "test@test.com"], wagerPhoto: UIImage(named: "gym"), goalDescription: "gym", wager: "Test Wager", deadline: "Test date", progress: 0.2, uuid: "005")
         wagers.append(newWager5)
     }
-
-func createAndSaveWager(owner: String, wagerBuddies: [String], wagerPhoto: UIImage?, goalDescription: String, wager: String, deadline: String, progress: Float, completion: @escaping (Result<Wager, DatabaseError>) -> Void) {
-    let newWager = Wager(owner: owner, wagerBuddies: wagerBuddies, wagerPhoto: wagerPhoto, goalDescription: goalDescription, wager: wager, deadline: deadline, progress: progress)
     
-    let wagersRef = db.collection(wagersCollection)
-    wagersRef.document("\(newWager.uuid)").setData([
-        "owner": "\(newWager.owner)",
-        "wagerBuddies": "\(newWager.wagerBuddies)",
-        "wagerPhoto": newWager.wagerPhoto?.jpegData(compressionQuality: 1),
-        "goalDescription": "\(newWager.goalDescription)",
-        "wager": "\(newWager.wager)",
-        "deadline": "\(newWager.deadline)",
-        "progress": "\(newWager.progress)"
-    ]) { error in
-        if let error = error {
-            print("Error adding document: \(error)")
-            return completion(.failure(.fireError(error)))
-        } else {
-            print("Wager Document added with ID: \(newWager.uuid)")
-            return completion(.success(newWager))
-        }
-    }
-}
-
-func fetchWager(uuid: String, completion: @escaping (Result<Wager, DatabaseError>) -> Void) {
-    let wagersRef = db.collection(wagersCollection)
-    
-    wagersRef.whereField("uuid", isEqualTo: uuid).getDocuments { (querySnapshot, error) in
-        if let error = error {
-            print("There was an error getting wagers with a uuid: \(uuid). - \(error)")
-            return completion(.failure(.fireError(error)))
-        } else {
-            guard let doc = querySnapshot!.documents.first,
-                  let fetchedWager = Wager(document: doc) else {
-                return completion(.failure(.couldNotUnwrap))
+    func createAndSaveWager(owner: String, wagerBuddies: [String], wagerPhoto: UIImage?, goalDescription: String, wager: String, deadline: String, progress: Float, completion: @escaping (Result<Wager, DatabaseError>) -> Void) {
+        let newWager = Wager(owner: owner, wagerBuddies: wagerBuddies, wagerPhoto: wagerPhoto, goalDescription: goalDescription, wager: wager, deadline: deadline, progress: progress)
+        
+        let wagersRef = db.collection(wagersCollection)
+        wagersRef.document("\(newWager.uuid)").setData([
+            "owner": "\(newWager.owner)",
+            "wagerBuddies": "\(newWager.wagerBuddies)",
+            "wagerPhoto": newWager.wagerPhoto?.jpegData(compressionQuality: 1),
+            "goalDescription": "\(newWager.goalDescription)",
+            "wager": "\(newWager.wager)",
+            "deadline": "\(newWager.deadline)",
+            "progress": "\(newWager.progress)"
+        ]) { error in
+            if let error = error {
+                print("Error adding document: \(error)")
+                return completion(.failure(.fireError(error)))
+            } else {
+                print("Wager Document added with ID: \(newWager.uuid)")
+                return completion(.success(newWager))
             }
-            return completion(.success(fetchedWager))
         }
     }
-}
+    
+    
+    func fetchAllWagersWithOwner(owner: String, completion: @escaping (Result<[Wager], DatabaseError>) -> Void) {
+        let wagerRef = db.collection(wagersCollection)
+        var wagers: [Wager] = []
+        wagerRef.getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("There was an error getting wagers with an owner: \(owner). - \(error)")
+                return completion(.failure(.fireError(error)))
+            } else {
+                guard let docs = querySnapshot?.documents
+                else {
+                    
+                    return completion(.failure(.couldNotUnwrap))
+                }
+                for document in docs {
+                    var wager = document.data()
+                    guard let owner = document["owner"] as? String,
+                          let wagerBuddies = document["wagerBuddies"] as? [String],
+                          let goalDescription = document["goalDescription"] as? String,
+                          let wager2 = document["wager"] as? String,
+                          let deadline = document["deadline"] as? String,
+                          let progress = document["progress"]as? Float,
+                          let acceptedWagers = document["acceptedWagers"]as? [Wager],
+                          let pendingWagers = document["pendingWagers"]as? [Wager] else {return completion(.failure(.couldNotUnwrap))}
+                    
+                    var wagerPhoto: UIImage?
+                    
+                    if let wagerPhotoData = document["wagerPhoto"] as? Data {
+                        wagerPhoto = UIImage(data: wagerPhotoData)
+                    }
+                    let currentWager = Wager(owner: owner, wagerBuddies: wagerBuddies, wagerPhoto: wagerPhoto, goalDescription: goalDescription, wager: wager2, deadline: deadline, progress: progress, uuid: document.documentID, acceptedWagers: acceptedWagers, pendingWagers: pendingWagers)
+                    wagers.append(currentWager)
+                }
+                return completion(.success(wagers))
+            }
+        }
+    }
+    
+    func fetchWager(uuid: String, completion: @escaping (Result<Wager, DatabaseError>) -> Void) {
+        let wagersRef = db.collection(wagersCollection)
+        
+        wagersRef.whereField("uuid", isEqualTo: uuid).getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("There was an error getting wagers with a uuid: \(uuid). - \(error)")
+                return completion(.failure(.fireError(error)))
+            } else {
+                guard let doc = querySnapshot!.documents.first,
+                      let fetchedWager = Wager(document: doc) else {
+                    return completion(.failure(.couldNotUnwrap))
+                }
+                return completion(.success(fetchedWager))
+            }
+        }
+    }
 }
--- accounta-bill-a-buddy/accounta-bill-a-buddy/Models/Wager.swift
+++ accounta-bill-a-buddy/accounta-bill-a-buddy/Models/Wager.swift
@@ -11,23 +11,25 @@ import Firebase
 class Wager {
     var uuid: String
     var owner: String
+    var acceptedWagers: [Wager]
+    var pendingWagers: [Wager]
     // a list of other people who are able to see/comment on the wager
     var wagerBuddies: [String]
     var wagerPhoto: UIImage? {
-            get {
-                guard let data = wagerPhotoData else {return nil}
-                return UIImage(data: data)
-            } set {
-                wagerPhotoData = newValue?.jpegData(compressionQuality: 0.5)
-            }
+        get {
+            guard let data = wagerPhotoData else {return nil}
+            return UIImage(data: data)
+        } set {
+            wagerPhotoData = newValue?.jpegData(compressionQuality: 0.5)
         }
+    }
     var wagerPhotoData: Data?
     var goalDescription: String
     var wager: String
     var deadline: String
     var progress: Float
     
-    init(owner: String, wagerBuddies: [String], wagerPhoto: UIImage? = nil, goalDescription: String, wager: String, deadline: String, progress: Float, uuid: String = UUID().uuidString) {
+    init(owner: String, wagerBuddies: [String], wagerPhoto: UIImage? = #imageLiteral(resourceName: "wagerDefaultPhoto"), goalDescription: String, wager: String, deadline: String, progress: Float, uuid: String = UUID().uuidString, acceptedWagers: [Wager] = [], pendingWagers: [Wager] = []) {
         self.owner = owner
         self.wagerBuddies = wagerBuddies
         self.goalDescription = goalDescription
@@ -35,7 +37,10 @@ class Wager {
         self.deadline = deadline
         self.progress = progress
         self.uuid = uuid
+        self.acceptedWagers = acceptedWagers
+        self.pendingWagers = pendingWagers
         self.wagerPhoto = wagerPhoto
+
     }
     
     convenience init?(document: DocumentSnapshot) {
@@ -44,15 +49,18 @@ class Wager {
               let goalDescription = document["goalDescription"] as? String,
               let wager = document["wager"] as? String,
               let deadline = document["deadline"] as? String,
-              let progress = document["progress"] as? Float else { return nil }
-
-               var wagerPhoto: UIImage?
-               
-               if let wagerPhotoData = document["wagerPhoto"] as? Data {
-                   wagerPhoto = UIImage(data: wagerPhotoData)
-               }
-               
-        self.init(owner: owner, wagerBuddies: wagerBuddies, wagerPhoto: wagerPhoto, goalDescription: goalDescription, wager: wager, deadline: deadline, progress: progress, uuid: document.documentID)
+              let progress = document["progress"] as? Float,
+              let acceptedWagers = document["acceptedWagers"] as? [Wager],
+              let pendingWagers = document["pendingWagers"] as? [Wager] else { return nil }
+        
+        
+        var wagerPhoto: UIImage?
+        
+        if let wagerPhotoData = document["wagerPhoto"] as? Data {
+            wagerPhoto = UIImage(data: wagerPhotoData)
+        }
+        
+        self.init(owner: owner, wagerBuddies: wagerBuddies, wagerPhoto: wagerPhoto, goalDescription: goalDescription, wager: wager, deadline: deadline, progress: progress, uuid: document.documentID, acceptedWagers: acceptedWagers, pendingWagers: pendingWagers)
     }
 }
 
