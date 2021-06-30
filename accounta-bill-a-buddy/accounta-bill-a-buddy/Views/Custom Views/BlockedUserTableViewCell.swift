d6bc6f6e076eb771673bf1787a00fb7701272955
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-06-30 14:19:36 -0700

    added user profile VC and blocked users VC

---
+++ accounta-bill-a-buddy/accounta-bill-a-buddy/Views/Custom Views/BlockedUserTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BlockedUserTableViewCell.swift
+//  accounta-bill-a-buddy
+//
+//  Created by Jenny Morales on 6/30/21.
+//
+
+import UIKit
+
+class BlockedUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}//End of class
